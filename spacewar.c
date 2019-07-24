b51b7225dcd854b7196a54f8876aac207e731ad1
ashishpavan <36131484+ashishpavan@users.noreply.github.com>
2019-07-24 22:15:26 +0530

    Add files via upload

---
+++ spacewar.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<graphics.h>
+#include<stdlib.h>
+void spaceship(int x,int y);
+void main()
+{
+	int i=0;
+	char positionx;
+	int gd=DETECT,gm;
+	initgraph(&gd,&gm,"C:\TURBOC3\BGI");
+	for(i=0;i<5;i++)
+	{
+		outtextxy(20,100,"bro");
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+
+}	
+	
+	
